7463716a198c26e182cf5922277d7aaa463478ed
yomu85 <yomu85@gmail.com>
2022-05-01 23:43:20 +0900

    540,541 추가, 메인 문구 수정

--- src/components/Headline.svelte
+++ src/components/Headline.svelte
@@ -4,8 +4,7 @@
   </h1>
   <div class="description">
     <ul>
-      <li>#1~#500: 2022년 2월 22일 판매가 종료되었습니다.</li>
-      <li>#501~#722: <a href="https://open.kakao.com/o/gMAFhdS" target="_blank">ICON 커뮤니티에서</a> <span class="primary">에어드랍 진행 중</span> 입니다.</li>
+      <li>#501~#541: 2022년 5월 1일 ICON 커뮤니티 대상으로 에어드랍 완료되었습니다.</li>
       <li>하단 검색을 통해 <span class="primary">contract 주소, 수집품 ID</span>를 복사하여 메타마스크 지갑에 NFT를 추가하세요.</li>
     </ul>
   </div>
